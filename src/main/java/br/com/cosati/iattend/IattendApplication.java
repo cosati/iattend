7a08a53c64d5658d9904757fadd16a0f60e98906
Alexandre Cosati <cosati.alexandre@gmail.com>
2020-04-14 20:30:39 -0300

    Projeto criado

---
+++ src/main/java/br/com/cosati/iattend/IattendApplication.java
@@ -0,0 +1,13 @@
+package br.com.cosati.iattend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IattendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IattendApplication.class, args);
+	}
+
+}
